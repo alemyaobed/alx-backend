a3e34328a32845377d455049c19ecdbb46f26abb
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-24 16:12:16 +0000

    correction to the type annotation for the return value

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,10 +9,10 @@ for those particular pagination parameters.
 
 Page numbers are 1-indexed, i.e. the first page is page 1.
 '''
-from typing import Union, Tuple
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[Union[int, int]]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
