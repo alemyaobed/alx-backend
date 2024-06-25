595242c66be54ebe9852c5f0e26c54b913c1e230
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-25 18:17:58 +0000

    smart way for the LIFOCache added as a comment

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -37,6 +37,29 @@ class LIFOCache(BaseCaching):
                 del self.cache_data[key_last]
             self.cache_data[key] = item
 
+    ###################################################################
+    # A SMARTER WAY AS OPPOSED TO ITERATING
+    ###################################################################
+    # def __init__(self):
+    #     """ Initialize the LIFOCache instance """
+    #     super().__init__()
+    #     self.last_key = None  # To keep track of the last added key
+
+    # def put(self, key, item):
+    #     """ Add an item in the cache """
+    #     if key is None or item is None:
+    #         return
+
+    #     # If the cache is full, remove the last added item
+    #     if len(self.cache_data) >= self.MAX_ITEMS:
+    #         if self.last_key is not None:
+    #             print(f"DISCARD: {self.last_key}")
+    #             del self.cache_data[self.last_key]
+
+    #     # Add the new item to cache
+    #     self.cache_data[key] = item
+    #     self.last_key = key
+
     def get(self, key):
         '''Get an item by key'''
         # Checking if the key is not None
