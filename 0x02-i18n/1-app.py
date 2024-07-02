98a49f21945d20fd3521cdaf52b9372589b2723b
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-02 19:27:16 +0000

    necessary implementation with babel-flask

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 ''' A flask app to be used for i18n and l10n'''
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    ''' Configuration class for Babel '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app=app)
 
 
 @app.route("/")
 def index():
-    ''' Renders the index template '''
-    return render_template("0-index.html")
+    ''' Renders the index template with babel featured '''
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
