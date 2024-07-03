2fa27e54e1efb4cfa86440e9c36d3bdb1cb35301
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-03 13:16:16 +0000

    the base implementation of task 5, awaiting the translation implementation

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ''' A flask app to be used for i18n and l10n'''
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
 
@@ -16,6 +16,29 @@ app.config.from_object(Config)
 babel = Babel(app=app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    ''' Gets a user from the mock user data above '''
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+@app.before_request
+def before_request():
+    ''' Executes this functionality before '''
+    logged_in_user = get_user()
+    if logged_in_user:
+        g.user = logged_in_user
+
+
 @babel.localeselector
 def get_locale():
     ''' The locale selector function '''
@@ -29,7 +52,7 @@ def get_locale():
 def index():
     ''' Renders the index template with babel featured '''
     locale = get_locale()
-    return render_template("4-index.html", locale=locale)
+    return render_template("5-index.html", locale=locale)
 
 
 if __name__ == "__main__":
