d24745a2fe6d36d8b4debeee626a07bda7eef2ce
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-08-01 01:13:17 +0000

    the implementation of the tests

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,15 +1,20 @@
+import kue from 'kue';
+
 export default function createPushNotificationsJobs(jobs, queue) {
   if (!Array.isArray(jobs)) {
     throw new Error('Jobs is not an array');
   }
 
   jobs.forEach((jobData) => {
-    const job = queue.create('push_notification_code_3', jobData)
-      .save((err) => {
-        if (!err) {
-          console.log(`Notification job created: ${job.id}`);
-        }
-      });
+    const job = queue.create('push_notification_code_3', jobData);
+
+    job.save((err) => {
+      if (err) {
+        console.error(`Job save failed: ${err}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
 
     job.on('complete', () => {
       console.log(`Notification job ${job.id} completed`);
